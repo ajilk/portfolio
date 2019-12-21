0bac0db3dd1434b707f27c3e23eac8fbb30a1cf1
Temurbek Sabirov <prodota150@gmail.com>
2019-12-21 01:35:45 -0500

    Updates

---
+++ precache-manifest.d84c8eec8eaae81d442da70de5483668.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e0153a3d06a9bc71c0c514d837b3c2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5575273d362ccf388faa",
+    "url": "/portfolio/static/css/2.7a6bef75.chunk.css"
+  },
+  {
+    "revision": "f83c6319e55e1f501d00",
+    "url": "/portfolio/static/css/main.d524fd39.chunk.css"
+  },
+  {
+    "revision": "5575273d362ccf388faa",
+    "url": "/portfolio/static/js/2.325722b5.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/portfolio/static/js/2.325722b5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f83c6319e55e1f501d00",
+    "url": "/portfolio/static/js/main.f729e740.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  },
+  {
+    "revision": "d5bf13c671e7cfd3c3487b756fa2ab2c",
+    "url": "/portfolio/static/media/Profile_pic.d5bf13c6.png"
+  },
+  {
+    "revision": "df137b674d25ed2ed075ebb99188409b",
+    "url": "/portfolio/static/media/hallway-pic.df137b67.jpg"
+  }
+]);
