b6ad70dbe6526c49cb46d0d84ac4e59435fe8aee
Temurbek Sabirov <prodota150@gmail.com>
2019-12-24 11:44:42 -0500

    Updates

---
+++ precache-manifest.329d2ebebdd3d90a3df526ccd043c458.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce866b354cfbffe3187135dee91daac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3c205d355a73d812a16",
+    "url": "/static/css/2.7a6bef75.chunk.css"
+  },
+  {
+    "revision": "ca6803c199cb995459fc",
+    "url": "/static/css/main.d524fd39.chunk.css"
+  },
+  {
+    "revision": "e3c205d355a73d812a16",
+    "url": "/static/js/2.e863de4b.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/static/js/2.e863de4b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ca6803c199cb995459fc",
+    "url": "/static/js/main.7b546bd7.chunk.js"
+  },
+  {
+    "revision": "00ccf1beb4d435cfbe60",
+    "url": "/static/js/runtime-main.c7cc6e23.js"
+  },
+  {
+    "revision": "d5bf13c671e7cfd3c3487b756fa2ab2c",
+    "url": "/static/media/Profile_pic.d5bf13c6.png"
+  },
+  {
+    "revision": "df137b674d25ed2ed075ebb99188409b",
+    "url": "/static/media/hallway-pic.df137b67.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1711d9ceda615eda9dc9bf63e610b143.js"
+  "/precache-manifest.329d2ebebdd3d90a3df526ccd043c458.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
