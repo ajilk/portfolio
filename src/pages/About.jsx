e695f37855c3db69a430c44b06a62167c3ec21e4
Temurbek Sabirov <prodota150@gmail.com>
2019-12-27 00:29:25 -0500

    update about information

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -18,18 +18,21 @@ class About extends Component {
               <h1>About</h1>
 
               <p>
-                My desire for learning new technology has been evolving ever
-                since I was in elementary school. I used to experiement with
-                random software I had on my Personal Computer. I did not know
-                what the purpose of software I was using was but it did not stop
-                me. I would tinker with the functionality until I got something
-                that looked like work or until i broke or crashed the program.
-                My brother would get angry at me everytime I crashed software
-                like Photoshop or Sony Vegas. However, failing is a part of
-                learning and as a result of failing in my early ages, I feel
-                confident using software to do what I want, including
-                video/photo editting or writing bits of code to create another
-                software.
+                I am a Computer Science student at Brooklyn College. Currently
+                in my 3rd year pursuing a career in Software Engineering. My
+                interest in technology started developing ever since I was a
+                tiny kid. My brother and I shared a PC where he would install
+                different photo/video editing software. Without having a single
+                clue on how to use the software, I would tinker with it either
+                until I ended up creating something or crashed it. My brother
+                would get upset over me breaking his software. However, I saw
+                this as an opportunity to learn something new. As a result of
+                all the tinkering I did in the past, I find it easy to learn how
+                to use new software that I have never seen before. Also, this
+                fact can be applied to programming language, libraries, and
+                frameworks. Since failing is considered as a part of learning,
+                my failed attempts in the past led me to have such passion for
+                technology.
               </p>
             </div>
           </Cell>
@@ -37,6 +40,20 @@ class About extends Component {
           <Cell col={12}>
             <div style={{ display: 'block', textAlign: 'left' }}>
               <h1>Interests</h1>
+              <h4>Programming</h4>
+              <p>
+                The idea of creating something out of nothing thrills me.
+                Programming gives me the skill and power to turn my dreams into
+                reality. If I think of creating an application to track my
+                calories, I can create it using various APIs and libraries. If I
+                want to see how many times I check Instagram in a day, I can
+                write an algorithm to record every time I go on Instagram. Some
+                projects can be done alone and for some projects you need a team
+                of good developers. Working in a team brings great benefits for
+                each memeber because they will have a change to learn from one
+                another. Although, it is not easy and takes a lot of effort and
+                skill, possibilities are limitless.
+              </p>
               <h4>Personal Fitness</h4>
               <p>
                 Wise men say: you need to take care of your mind as well as your
